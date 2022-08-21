6c7086f0b63c15485be57be37ce6a12c9a80c1ba
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-08-22 02:02:08 +0300

    lab complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+    
+    def initialize (word)
+        @word = word
+    end
+
+    def match(array)
+        @array = array
+        new_word = word_sort(@word)
+        p word = @array.filter {
+      |item| word_sort(item) == new_word
+    }
+       
+    end
+
+    def word_sort word
+        word.chars.sort
+    end
+end
